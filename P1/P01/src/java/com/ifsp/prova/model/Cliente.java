d0940c694f7d3c318e1426f38660dc004cd6f430
Daniel Souza <danielfss98@gmail.com>
2018-09-23 14:33:17 -0300

    Add class cliente

---
+++ P1/P01/src/java/com/ifsp/prova/model/Cliente.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ifsp.prova.model;
+
+/**
+ *
+ * @author aluno
+ */
+public class Cliente {
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getDt_ultima_operacao() {
+        return dt_ultima_operacao;
+    }
+
+    public void setDt_ultima_operacao(String dt_ultima_operacao) {
+        this.dt_ultima_operacao = dt_ultima_operacao;
+    }
+
+    public String getDs_ultima_operacao() {
+        return ds_ultima_operacao;
+    }
+
+    public void setDs_ultima_operacao(String ds_ultima_operacao) {
+        this.ds_ultima_operacao = ds_ultima_operacao;
+    }
+
+    int id;
+    String nome;
+    String endereco;
+    int cpf;
+    double saldo;
+    String dt_ultima_operacao;
+    String ds_ultima_operacao;
+
+}
