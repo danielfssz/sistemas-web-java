b5d339ff638d384782b4d651f868f16ffe619cbe
Daniel Souza <daniel.souza@modalgr.com.br>
2018-12-07 22:15:04 -0200

    Arquivos iniciais

---
+++ Knockout/src/java/com/ifsp/edu/model/Produto.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ifsp.edu.model;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Produto {
+
+    private int id;
+    private String codigo;
+    private String descricao;
+    private double valor;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+}
